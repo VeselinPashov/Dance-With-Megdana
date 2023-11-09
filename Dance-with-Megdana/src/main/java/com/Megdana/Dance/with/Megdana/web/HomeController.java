a70139f3aa3efb0088a6e23ad959e4c4c2bb6163
Veselin Pashov <pashovip@gmail.com>
2023-11-09 22:15:22 +0200

    Added the first possibility to pass dynamic data from controller to Thymeleaf

--- Dance-with-Megdana/src/main/java/com/Megdana/Dance/with/Megdana/web/HomeController.java
+++ Dance-with-Megdana/src/main/java/com/Megdana/Dance/with/Megdana/web/HomeController.java
@@ -1,6 +1,8 @@
 package com.Megdana.Dance.with.Megdana.web;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,7 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
     @GetMapping("/")
-    public String getHome (ModelAndView modelAndView) {
+    public String getHome (Model model) {
+        model.addAttribute("message", "Y U not working?");
+        model.addAttribute("url", "test URL");
         return "home";
+    }
+
+    @GetMapping("/test")
+    public String getTest (Model model) {
+        String textAttribute = "Is this gona work someday?";
+        model.addAttribute("text", textAttribute);
+        return "test";
     }
 }
